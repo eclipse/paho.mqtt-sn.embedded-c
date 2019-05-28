6cb7935027e36995527211eb6f589b4ee1121786
tomoaki <tomoaki@tomy-tech.com>
2019-05-28 16:31:52 +0900

    BugFix of #151
    Signed-off-by: tomoaki <tomoaki@tomy-tech.com>

--- MQTTSNGateway/src/linux/udp6/SensorNetwork.cpp
+++ MQTTSNGateway/src/linux/udp6/SensorNetwork.cpp
@@ -65,24 +65,52 @@ void SensorNetAddress::setAddress(struct sockaddr_in6 *IpAddr, uint16_t port)
 
 /**
  *  convert Text data to SensorNetAddress
- *  @param  buf is pointer of IP_Address:PortNo format text
+ *  @param  data is a IP_Address:PortNo format string
  *  @return success = 0,  Invalid format = -1
  */
 int SensorNetAddress::setAddress(string* data)
 {
-	const char *cstr = data->c_str();
-	inet_pton(AF_INET6, cstr, &(_IpAddr.sin6_addr));
-	return 0;
+
+	size_t pos = data->find_last_of(":");
+
+	if ( pos != string::npos)
+	{
+		int portNo = 0;
+		string port = data->substr(pos + 1);
+
+		if ( ( portNo = atoi(port.c_str()) ) > 0 )
+		{
+			_portNo = htons(portNo);
+
+			string ip = data->substr(1,pos - 1);
+			const char *cstr = ip.c_str();
+
+			if (inet_pton(AF_INET6, cstr, &(_IpAddr.sin6_addr)) == 1 )
+			{
+				return 0;
+			}
+		}
+	}
+	_portNo = 0;
+	memset((void *)&_IpAddr,0,sizeof(_IpAddr));
+	return -1;
 }
+
 /**
  *  convert Text data to SensorNetAddress
- *  @param  buf is pointer of IP_Address:PortNo format text
+ *  @param  data is pointer of IP_Address format text
  *  @return success = 0,  Invalid format = -1
  */
 int SensorNetAddress::setAddress(const char* data)
 {
-	inet_pton(AF_INET6, data, &(_IpAddr.sin6_addr));
-	return 0;
+	if ( inet_pton(AF_INET6, data, &(_IpAddr.sin6_addr)) == 1 )
+	{
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 char* SensorNetAddress::getAddress(void)
