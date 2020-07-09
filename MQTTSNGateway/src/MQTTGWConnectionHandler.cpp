ab94a094b543b1b24c28ab58d741ffb928cec48f
Ian Craggs <icraggs@gmail.com>
2020-07-09 12:06:20 +0100

    Update copyright statement

--- MQTTSNGateway/src/MQTTGWConnectionHandler.cpp
+++ MQTTSNGateway/src/MQTTGWConnectionHandler.cpp
@@ -1,5 +1,5 @@
 /**************************************************************************************
- * Copyright (c) 2016, Tomoaki Yamaguchi
+ * Copyright (c) 2016, 2020 Tomoaki Yamaguchi and others
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
