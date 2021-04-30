9a84cc9a6a0cea2bccc8fcf58f099438296e0ef5
tomoaki <tomoaki@tomy-tech.com>
2021-04-30 16:18:14 +0900

    Bugfix  #276 failed
    Signed-off-by: tomoaki <tomoaki@tomy-tech.com>

--- travis-build.sh
+++ travis-build.sh
@@ -9,3 +9,13 @@ echo "travis build dir $TRAVIS_BUILD_DIR pwd $PWD"
 cmake ..
 make
 ctest -VV --timeout 600
+cmake .. -DSENSORNET=xbee
+make MQTT-SNGateway
+cmake .. -DSENSORNET=udp6
+make MQTT-SNGateway
+cmake .. -DSENSORNET=loralink
+make MQTT-SNGateway
+make test
+cd MQTTSMGateway/GatewayTester
+make
+
