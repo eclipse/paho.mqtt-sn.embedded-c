c4708b81f7eefa0ac7fd8eae9084b644933782e8
Niklas Lantau <niklaslantau@gmail.com>
2022-05-23 15:20:08 +0200

    code-style: using 4 space standard
    Changed from tabs/space-mix till 4 space standard.
    
    Signed-off-by: Niklas Lantau <niklaslantau@gmail.com>

--- MQTTSNPacket/samples/linux/rfcomm/qos0pub.c
+++ MQTTSNPacket/samples/linux/rfcomm/qos0pub.c
@@ -28,26 +28,26 @@
 
 int main(int argc, char** argv)
 {
-	int rc = 0;
-	unsigned char buf[200];
-	int buflen = sizeof(buf);
-	MQTTSN_topicid topic;
-	unsigned char* payload = (unsigned char*)"mypayload";
-	int payloadlen = strlen((char*)payload);
-	int len = 0;
-	int dup = 0;
-	int qos = 0;
-	int retained = 0;
-	short packetid = 0;
-//	char *topicname = "a long topic name";
+    int rc = 0;
+    unsigned char buf[200];
+    int buflen = sizeof(buf);
+    MQTTSN_topicid topic;
+    unsigned char* payload = (unsigned char*)"mypayload";
+    int payloadlen = strlen((char*)payload);
+    int len = 0;
+    int dup = 0;
+    int qos = 0;
+    int retained = 0;
+    short packetid = 0;
+//  char *topicname = "a long topic name";
     char *host = "";
     int channel = 1;
-	MQTTSNPacket_connectData options = MQTTSNPacket_connectData_initializer;
+    MQTTSNPacket_connectData options = MQTTSNPacket_connectData_initializer;
 
-	if (argc > 1)
-		host = argv[1];
+    if (argc > 1)
+        host = argv[1];
 
-	if (argc > 2)
+    if (argc > 2)
         channel = atoi(argv[2]);
 
     printf("Sending to address %s port %d\n", host, channel);
@@ -56,38 +56,38 @@ int main(int argc, char** argv)
         goto exit;
     }
 
-	options.clientID.cstring = "myclientid";
-	len = MQTTSNSerialize_connect(buf, buflen, &options);
+    options.clientID.cstring = "myclientid";
+    len = MQTTSNSerialize_connect(buf, buflen, &options);
     rc = rfcomm_sendPacketBuffer(buf, len);
 
-	/* wait for connack */
+    /* wait for connack */
     if (MQTTSNPacket_read(buf, buflen, rfcomm_getdata) == MQTTSN_CONNACK)
-	{
-		int connack_rc = -1;
+    {
+        int connack_rc = -1;
 
-		if (MQTTSNDeserialize_connack(&connack_rc, buf, buflen) != 1 || connack_rc != 0)
-		{
-			printf("Unable to connect, return code %d\n", connack_rc);
-			goto exit;
-		}
-		else 
-			printf("connected rc %d\n", connack_rc);
-	}
-	else
-		goto exit;
+        if (MQTTSNDeserialize_connack(&connack_rc, buf, buflen) != 1 || connack_rc != 0)
+        {
+            printf("Unable to connect, return code %d\n", connack_rc);
+            goto exit;
+        }
+        else
+            printf("connected rc %d\n", connack_rc);
+    }
+    else
+        goto exit;
 
-	 
-	/* publish with short name */
-	topic.type = MQTTSN_TOPIC_TYPE_SHORT;
-	memcpy(topic.data.short_name, "tt", 2);
-	len = MQTTSNSerialize_publish(buf, buflen, dup, qos, retained, packetid,
-			topic, payload, payloadlen);
+     
+    /* publish with short name */
+    topic.type = MQTTSN_TOPIC_TYPE_SHORT;
+    memcpy(topic.data.short_name, "tt", 2);
+    len = MQTTSNSerialize_publish(buf, buflen, dup, qos, retained, packetid,
+            topic, payload, payloadlen);
     rc = rfcomm_sendPacketBuffer(buf, len);
 
-	printf("rc %d from send packet for publish length %d\n", rc, len);
+    printf("rc %d from send packet for publish length %d\n", rc, len);
 
 exit:
     rfcomm_close();
 
-	return 0;
+    return 0;
 }
